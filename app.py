22cdb698f8f737e1f85fe5ff3d1ce3eef074d318
Sabharish <sabharish@student.tce.edu>
2026-06-13 20:43:19 +0530

    Fix HuggingFace embeddings

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
